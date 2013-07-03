8b5c98d6c42507183bb71b5e4fdcbd1f6d4b9a85
corey jergensen <corey.jergensen@gmail.com>
2013-07-03 14:29:49 -0500

    additional refactors to abstract functionality
    [index.html]
    - updated card display value to use the displayLanguage field of the
    currentWord object
    [js/cartas.js]
    - extracted inline function defenitions to prototype for AppViewModel
    - AppViewModel now accepts a source and target language as arguments
    - displayLanguage is an observable field
    - moved card display value to template

--- js/cartas.js
+++ js/cartas.js
@@ -1,9 +1,6 @@
 var vocab = spanishVocab;
 var vocabList = vocab.verbs;
 
-var sourceLanguage = 'english';
-var targetLanguage = 'spanish';
-
 function getRandomWord() {
     var index = getRandomInt(0, vocabList.length - 1);
     return vocabList[index];
@@ -13,16 +10,20 @@ function getRandomInt (min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function AppViewModel() {
-    var currWord = getRandomWord();
-    this.cardWord = ko.observable(currWord[targetLanguage]);
-    this.toggleCardWord = function() {
-        this.cardWord(currWord[sourceLanguage]);
-    }
-    this.nextWord = function() {
-        currWord = getRandomWord();
-        this.cardWord(currWord[targetLanguage]);
-    }
+function AppViewModel(sourceLanguage, targetLanguage) {
+    this.sourceLanguage = sourceLanguage;
+    this.targetLanguage = targetLanguage;
+    this.displayLanguage = ko.observable(sourceLanguage);
+
+    this.currentWord = ko.observable(getRandomWord());
 }
 
-ko.applyBindings(new AppViewModel());
+AppViewModel.prototype.toggleDisplayLanguage = function() {
+    this.displayLanguage(this.displayLanguage() == this.sourceLanguage ? this.targetLanguage : this.sourceLanguage);
+};
+
+AppViewModel.prototype.nextWord = function () {
+        this.currentWord(getRandomWord());
+};
+
+ko.applyBindings(new AppViewModel('english', 'spanish'));
